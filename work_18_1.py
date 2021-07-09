d23e27f7198b63da13dfe399ded74269ce9e8990
rudbog011 <rudbog011@gmail.com>
2021-07-09 18:54:24 +0300

    work_18

---
+++ work_18_1.py
@@ -0,0 +1,24 @@
+def My_Tester():
+    question = [
+        'Сколько будет два плюс два умноженное на два?',
+        'Бревно нужно распилить на 10 частей, сколько надо сделать распилов?',
+        'На двух руках 10 пальцев. Сколько пальцев на 5 руках?',
+        'Укол делают каждые полчаса, сколько нужно минут для трех уколов?',
+        'Пять свечей горело, две потухло. Сколько свечей осталось?'
+    ]
+    answer = [6, 9, 25, 60, 2]
+    count_right_answer = 0
+    for i in range(len(question)):
+        print(question[i])
+        user_answer = int(input())
+        right_answer = answer[i]
+        if user_answer == right_answer:
+            count_right_answer += 1
+    print('Количество правильных ответов=', count_right_answer)
+    print('Хотите пройти My-Tester заново? (Да/Нет)')
+    answer_user = input()
+    if answer_user == 'Да':
+        My_Tester()
+    else:
+        print('До новых встреч!')
+My_Tester()
---
+++ work_18_3.py
@@ -0,0 +1,25 @@
+def My_Tester():
+    question = [
+        'Сколько будет два плюс два умноженное на два?',
+        'Бревно нужно распилить на 10 частей, сколько надо сделать распилов?',
+        'На двух руках 10 пальцев. Сколько пальцев на 5 руках?',
+        'Укол делают каждые полчаса, сколько нужно минут для трех уколов?',
+        'Пять свечей горело, две потухло. Сколько свечей осталось?'
+    ]
+    answer = [6, 9, 25, 60, 2]
+    result = ['Идиот', 'Кретин', 'Дурак', 'Нормальный', 'Талант', 'Гений']
+    count_right_answer = 0
+    for i in range(len(question)):
+        print('№', i + 1, ') ', question[i], sep='')
+        user_answer = int(input())
+        right_answer = answer[i]
+        if user_answer == right_answer:
+            count_right_answer += 1
+    print('Ваш результат:', result[count_right_answer])
+    print('Хотите пройти My-Tester заново? (Да/Нет)')
+    answer_user = input()
+    if answer_user == 'Да':
+        My_Tester()
+    else:
+        print('До новых встреч!')
+My_Tester()
